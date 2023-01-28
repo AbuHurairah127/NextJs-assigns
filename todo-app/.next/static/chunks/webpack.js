41890adea41de3e80ca17567da17f09e12bcaafb
abuhurairah127 <abuhurairahmuhammadshafique@gmail.com>
2023-01-28 22:05:53 +0500

    signupform done

--- todo-app/.next/static/chunks/webpack.js
+++ todo-app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0130e3286e34260e"; }
+/******/ 		__webpack_require__.h = function() { return "13d8c5f6f1a3fae5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- todo-app/src/pages/auth/signup.tsx
+++ todo-app/src/pages/auth/signup.tsx
@@ -1,71 +1,95 @@
-import { FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
+import { FormControl, FormLabel, Input, Stack, Button } from "@chakra-ui/react";
+import { useState } from "react";
 import Link from "next/link";
+const SignUp = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    username: "",
+    email: "",
+    password: "",
+  });
 
-function SignupForm() {
-  const handleSubmit = (event: any) => {
-    event.preventDefault();
-    // handle form submission
+  const handleChange = (e: any) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    // add your login logic here
+    console.log(formData);
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <FormControl>
-        <FormLabel htmlFor="username">Username</FormLabel>
-        <Input
-          id="username"
-          name="username"
-          type="text"
-          placeholder="Enter your username"
-        />
-      </FormControl>
-
-      <FormControl>
-        <FormLabel htmlFor="password">Password</FormLabel>
-        <Input
-          id="password"
-          name="password"
-          type="password"
-          placeholder="Enter your password"
-        />
-      </FormControl>
-
-      <FormControl>
-        <FormLabel htmlFor="firstName">First Name</FormLabel>
-        <Input
-          id="firstName"
-          name="firstName"
-          type="text"
-          placeholder="Enter your first name"
-        />
-      </FormControl>
-
-      <FormControl>
-        <FormLabel htmlFor="lastName">Last Name</FormLabel>
-        <Input
-          id="lastName"
-          name="lastName"
-          type="text"
-          placeholder="Enter your last name"
-        />
-      </FormControl>
-
-      <FormControl>
-        <FormLabel htmlFor="email">Email</FormLabel>
-        <Input
-          id="email"
-          name="email"
-          type="email"
-          placeholder="Enter your email"
-        />
-      </FormControl>
-
-      <Button mt={4} variant="teal" type="submit">
-        Sign up
-      </Button>
-      <Button mt={4} variant="teal" type="submit">
-        <Link href="/auth/login">Login</Link>
-      </Button>
+      <Stack spacing={4}>
+        <FormControl>
+          <FormLabel htmlFor="firstName">First Name</FormLabel>
+          <Input
+            id="firstName"
+            name="firstName"
+            type="text"
+            value={formData.firstName}
+            onChange={handleChange}
+            placeholder="Enter your first name"
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel htmlFor="lastName">Last Name</FormLabel>
+          <Input
+            id="lastName"
+            name="lastName"
+            type="text"
+            value={formData.lastName}
+            onChange={handleChange}
+            placeholder="Enter your last name"
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel htmlFor="username">Username</FormLabel>
+          <Input
+            id="username"
+            name="username"
+            type="text"
+            value={formData.username}
+            onChange={handleChange}
+            placeholder="Enter your username"
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel htmlFor="email">Email</FormLabel>
+          <Input
+            id="email"
+            name="email"
+            type="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="Enter your email"
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel htmlFor="password">Password</FormLabel>
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            value={formData.password}
+            onChange={handleChange}
+            placeholder="Enter your password"
+          />
+        </FormControl>
+        <Button variant="teal" type="submit">
+          SignUp
+        </Button>
+        <Button variant="teal" type="submit">
+          <Link href="/auth/login">Login</Link>
+        </Button>
+      </Stack>
     </form>
   );
-}
-export default SignupForm;
+};
+
+export default SignUp;
