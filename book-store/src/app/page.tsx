34edf809f5013d36ead736fe3bbaad15b2847766
Abuhurairah127 <abuhurairahmuhammadshafique@gmail.com>
2023-03-26 10:05:11 +0500

    hero section ready

--- book-store/src/app/page.tsx
+++ book-store/src/app/page.tsx
@@ -1,19 +1,27 @@
 import Header from "@/components/header/Header";
 import BooksTable from "@/sections/booksTable/BooksTable";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import Image from "next/image";
+import heroBg from "./../assets/wave-haikei.svg";
+import HeroRightSection from "@/components/heroRightSection/HeroRightSection";
 
 export default function Home() {
   return (
     <main className={" min-h-screen"}>
       <Header />
-      <div>
-        <h1 className="mx-5 text-3xl font-thin tracking-wider text-[#302b63] text-center mt-5">
+      {/* <div style={{ backgroundImage: `url(${heroBg})` }}> */}
+      <Image
+        src={heroBg}
+        alt={"Hero Bg"}
+        className={"w-screen absolute -z-50"}
+      />
+      {/* </div> */}
+      <div className="flex justify-between items-center p-5 h-[55vh]">
+        <h1 className="mx-5 text-3xl font-thin tracking-wider text-white mt-5 w-screen lg:w-[50vw] text-center">
           Order Books: Unleash Your Imagination, One Page at a Time!
         </h1>
-        <BooksTable />
+        <HeroRightSection />
       </div>
+      <BooksTable />
     </main>
   );
 }
--- book-store/src/sections/booksTable/BooksTable.tsx
+++ book-store/src/sections/booksTable/BooksTable.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const BooksTable = () => {
-  return <div className="my-8">BooksTable</div>;
+  return (
+    <table className="my-8 border border-[#302b63]">
+      <thead>
+        <tr>
+          <td>Index No.</td>
+          <td></td>
+        </tr>
+      </thead>
+    </table>
+  );
 };
 
 export default BooksTable;
