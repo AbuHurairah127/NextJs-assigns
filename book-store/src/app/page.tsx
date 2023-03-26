2636bb8c4761888bd6ff474411f5c055815b3e27
Abuhurairah127 <abuhurairahmuhammadshafique@gmail.com>
2023-03-26 13:38:21 +0500

    some cahnges

--- book-store/src/app/page.tsx
+++ book-store/src/app/page.tsx
@@ -1,27 +1,41 @@
 import Header from "@/components/header/Header";
-import BooksTable from "@/sections/booksTable/BooksTable";
+import BooksTable from "../sections/booksTable/BooksTable";
 import Image from "next/image";
 import heroBg from "./../assets/wave-haikei.svg";
 import HeroRightSection from "@/components/heroRightSection/HeroRightSection";
+export type BookType = {
+  id: number;
+  name: string;
+  type: string;
+  available: boolean;
+};
+async function getData() {
+  const res = await fetch("https://simple-books-api.glitch.me/books");
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
 
-export default function Home() {
+  return res.json();
+}
+export default async function Home() {
+  const books: BookType[] = await getData();
+  console.log("🚀 ~ file: page.tsx:16 ~ Home ~ books:", books);
   return (
-    <main className={" min-h-screen"}>
+    <main className={" min-h-screen overflow-x-hidden"}>
       <Header />
-      {/* <div style={{ backgroundImage: `url(${heroBg})` }}> */}
-      <Image
-        src={heroBg}
-        alt={"Hero Bg"}
-        className={"w-screen absolute -z-50"}
-      />
-      {/* </div> */}
-      <div className="flex justify-between items-center p-5 h-[55vh]">
+
+      <div className="flex flex-col lg:flex-row justify-center lg:justify-between items-center p-5 h-[55vh] bg-gradient-to-r from-[#0f0c29] via-[#302b63] to-[#24243e]">
         <h1 className="mx-5 text-3xl font-thin tracking-wider text-white mt-5 w-screen lg:w-[50vw] text-center">
           Order Books: Unleash Your Imagination, One Page at a Time!
         </h1>
         <HeroRightSection />
       </div>
-      <BooksTable />
+      <h3 className="text-2xl font-thin text-[#302b63] text-center my-5 mx-5">
+        Books you need to Enhance your knowledge!
+      </h3>
+      <div className="flex justify-center ">
+        <BooksTable books={books} />
+      </div>
     </main>
   );
 }
