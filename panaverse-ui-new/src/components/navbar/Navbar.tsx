7a2eb7d48e6855853262e092d1507a750ba306a4
Abuhurairah127 <abuhurairahmuhammadshafique@gmail.com>
2023-04-05 20:12:26 +0500

    responsive navbar ready

--- panaverse-ui-new/src/components/navbar/Navbar.tsx
+++ panaverse-ui-new/src/components/navbar/Navbar.tsx
@@ -21,24 +21,22 @@ const Navbar = () => {
   const [isNavbar, setIsNavbar] = useState<boolean>(false);
 
   return (
-    <div className="h-20 flex justify-between items-center px-5">
-      <Image src={logo} alt="Panaverse DAO logo" className="w-11 h-11" />
-      <div></div>
-      <nav className="hidden  w-[50vw] lg:flex justify-between">
-        {Links.map((link, i) => (
-          <NavLinks key={i} name={link.name} path={link.path} />
-        ))}
-      </nav>
-      <a
-        className={
-          "bg-red-700 text-white py-4 px-12 rounded-md text-lg cursor-pointer transition-all duration-500 hover:bg-red-600 hidden lg:block"
-        }
-      >
-        Start Your Journey
-      </a>
-      {/* Smaller Devices Navbar */}
-      <div className="lg:hidden">
-        <div className="z-50">
+    <>
+      <div className="h-20 flex justify-between items-center px-5 z-50 bg-white">
+        <Image src={logo} alt="Panaverse DAO logo" className="w-11 h-11" />
+        <nav className="hidden  w-[50vw] lg:flex justify-between">
+          {Links.map((link, i) => (
+            <NavLinks key={i} name={link.name} path={link.path} />
+          ))}
+        </nav>
+        <a
+          className={
+            "bg-red-700 text-white py-4 px-12 rounded-md text-lg cursor-pointer transition-all duration-500 hover:bg-red-600 hidden lg:block"
+          }
+        >
+          Start Your Journey
+        </a>
+        <div className="lg:hidden z-50">
           <Hamburger
             toggled={isNavbar}
             color="#9d0208"
@@ -46,9 +44,24 @@ const Navbar = () => {
             onToggle={() => setIsNavbar(!isNavbar)}
           />
         </div>
-        <div className=""></div>
       </div>
-    </div>
+      <div
+        className={
+          isNavbar
+            ? "lg:hidden fixed left-0 top-20  transition-transform  w-screen z-0 bg-white flex flex-col items-center"
+            : "lg:hidden fixed left-0 top-0 transition-transform -translate-y-full duration-500 w-screen bg-white flex flex-col items-center"
+        }
+      >
+        {" "}
+        <nav className="flex flex-col  justify-between">
+          {Links.map((link, i) => (
+            <div className="py-3 text-center border w-screen">
+              <NavLinks key={i} name={link.name} path={link.path} />
+            </div>
+          ))}
+        </nav>
+      </div>
+    </>
   );
 };
 
