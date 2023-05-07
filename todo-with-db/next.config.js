e994ac103290beb53ca9f0a16d1e551b76eb7976
Abuhurairah127 <abuhurairahmuhammadshafique@gmail.com>
2023-05-07 10:58:38 +0500

    env var's added

--- todo-with-db/next.config.js
+++ todo-with-db/next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    POSTGRES_URL:
+      "postgres://default:CT1HKgV8NBSe@ep-little-wave-996844-pooler.ap-southeast-1.postgres.vercel-storage.com:5432/verceldb",
+    POSTGRES_PRISMA_URL:
+      "postgres://default:CT1HKgV8NBSe@ep-little-wave-996844-pooler.ap-southeast-1.postgres.vercel-storage.com:5432/verceldb?pgbouncer=true&connect_timeout=15",
+    POSTGRES_URL_NON_POOLING:
+      "postgres://default:CT1HKgV8NBSe@ep-little-wave-996844.ap-southeast-1.postgres.vercel-storage.com:5432/verceldb",
+    POSTGRES_USER: "default",
+    POSTGRES_HOST:
+      "ep-little-wave-996844-pooler.ap-southeast-1.postgres.vercel-storage.com",
+    POSTGRES_PASSWORD: "CT1HKgV8NBSe",
+    POSTGRES_DATABASE: "verceldb",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
